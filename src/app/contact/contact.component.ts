ffcf10aa8e8e043fad8d6b0e1c467db8fec70d50
laurac <laurac0362@gmail.com>
2024-11-19 09:49:47 +0100

    formulaire contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'] // Corrigé : "styleUrl" devient "styleUrls"
 })
 export class ContactComponent {
-title="Contact"
+  title = "Contact";
+
+  formData = {
+    nom: '',
+    prenom: '',
+    telephone: '',
+    email: '',
+  };
+
+  onSubmit() {
+    console.log('Formulaire soumis avec les données suivantes :', this.formData);
+    alert('Votre message a été envoyé avec succès !');
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.formData = {
+      nom: '',
+      prenom: '',
+      telephone: '',
+      email: '',
+    };
+  }
 }
