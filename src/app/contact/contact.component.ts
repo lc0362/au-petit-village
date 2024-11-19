b3e0c199b095dbe81186ba8dbc71bd05e1d78ff7
laurac <laurac0362@gmail.com>
2024-11-19 12:14:18 +0100

    ajout faq dans contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,30 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css'] // Corrigé : "styleUrl" devient "styleUrls"
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-  title = "Contact";
-
+  title = 'Contact';
   formData = {
     nom: '',
     prenom: '',
     telephone: '',
     email: '',
+    message: '',
   };
 
-  onSubmit() {
-    console.log('Formulaire soumis avec les données suivantes :', this.formData);
-    alert('Votre message a été envoyé avec succès !');
-    this.resetForm();
+  showError = false; 
+  faqState = [false, false, false, false, false]; // les réponses FAQ sont ne sont pas affichées par défaut donc état false au départ
+
+  toggleFaq(index: number) { // méthode toggle pour basculer de vrai à faux en fonction de si la question est cliquée. Le paramètre index représente la question cliquée (de 0 à 4 dans le html) 
+    this.faqState[index] = !this.faqState[index]; 
   }
 
-  resetForm() {
+  onSubmit() { // méthode onSubmit, si un des champs est vide au moment de soumettre le formulaire alors le showError passe à true
+    if (
+      !this.formData.nom ||
+      !this.formData.prenom ||
+      !this.formData.telephone ||
+      !this.formData.email ||
+      !this.formData.message
+    ) {
+      this.showError = true; 
+      return;
+    }
+
+    alert('Votre message a bien été envoyé !');
+
     this.formData = {
       nom: '',
       prenom: '',
       telephone: '',
       email: '',
-    };
+      message: '',
+    }; 
+    this.showError = false; 
   }
 }
+
